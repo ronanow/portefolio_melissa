761d6a664893128705908443c2c75f805bb45601
Your Name <you@example.com>
2026-03-14 17:38:25 +0100

    add whitenoise

--- src/portefolio_melissa/settings.py
+++ src/portefolio_melissa/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ← ajouté
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
